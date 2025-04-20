e4b6a20ccf4e715b25fa533d1cd197f18edba0a1
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-21 02:51:25 +0900

    feat: lv6 ManagerService 예외 메세지 수정

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -36,7 +36,7 @@ public class ManagerService {
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         if (todo.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
-            throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
+            throw new InvalidRequestException("담당자를 등록하려고 하는 유저 또는 일정을 만든 유저가 유효하지 않습니다.");
         }
 
         User managerUser = userRepository.findById(managerSaveRequest.getManagerUserId())
